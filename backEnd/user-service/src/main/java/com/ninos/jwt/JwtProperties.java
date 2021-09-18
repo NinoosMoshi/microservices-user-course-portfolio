982dedf100ef4a06591200bbacfb49ebe6244854
NinoosMoshi <nalkaton@gmail.com>
2021-09-18 06:56:44 -0400

    Spring jwtLogin class and properties Closes #4

---
+++ backEnd/user-service/src/main/java/com/ninos/jwt/JwtProperties.java
@@ -0,0 +1,8 @@
+package com.ninos.jwt;
+
+public class JwtProperties {
+    public static final String SECRET = "MY-SECRET-WORD";
+    public static final int EXPIRATION_TIME = 864_000_000;  // 10 days
+    // public static final String TOKEN_PREFIX = "Bearer ";
+}
+
